33b6701f60a55d1f721efe1940f0b3438e0cc681
tyler <tylerfinleyy@Yahoo.com>
2015-08-07 12:21:40 -0700

    changed codes

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,5 +1,17 @@
 class String
-  define_method(:word_count) do |comp|
-    comp.downcase().scan(/(?=#{self.downcase()})/).count
+  define_method(:word_count) do
+    print ' what is your string? '
+    input = gets.chomp
+    print ' what word are you looking for?'
+    input1 = gets
+    input1 = chomp
+    input.scan(input1)
+
+    count = 0
+    if input.include?(input1)
+
+    count = count.+(1)
+    end
+    count
   end
 end
